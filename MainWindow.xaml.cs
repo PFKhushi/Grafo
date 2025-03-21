e27263e3d44586a03bdb91f01569fc6719cd90eb
Pedro Firmino <mafiazuka@gmail.com>
2025-03-20 22:20:56 -0300

    Grafo estatico sendo impresso

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,16 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+// using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+using Microsoft.Msagl.WpfGraphControl;
+using Label = System.Windows.Controls.Label;
 
 namespace Grafo
 {
@@ -22,7 +28,29 @@ namespace Grafo
     {
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Graph g = new Graph("graph");
+
+            g.AddNode("A");
+            g.AddNode("B");
+            g.AddNode("C");
+            g.AddNode("D");
+            g.AddNode("E");
+            g.AddNode("F");
+
+            g.AddEdge("A", "B");
+            g.AddEdge("A", "C");
+            g.AddEdge("B", "D");
+            g.AddEdge("B", "E");
+            g.AddEdge("C", "F");
+
+            gViewer.Graph = g;
+
         }
     }
 }
